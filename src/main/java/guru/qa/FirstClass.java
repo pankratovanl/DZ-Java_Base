7f3462bef2e297a6d316c9e64e16fbb736dfd65a
Pankratova Nadezhda <pankratovanl@gmail.com>
2022-03-07 22:47:59 +0500

    added Java FirstClass

---
+++ src/main/java/guru/qa/FirstClass.java
@@ -0,0 +1,44 @@
+package guru.qa;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        int a = 10, b = 20, c = 30, d = 35;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(b / a);
+
+        System.out.println(d % c);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(c--);
+        System.out.println(--c);
+
+        System.out.println(b > c);
+        System.out.println(a < c);
+        System.out.println(a + b >= d);
+        System.out.println(d - a <= c);
+        System.out.println((a + b) == c);
+        System.out.println(a != b);
+
+        System.out.println(a += b);
+        System.out.println(c -= a);
+
+        System.out.println(false & true | true);
+        System.out.println(true | false);
+
+        byte iByte = 120;
+        short iShort = -32_768;
+        long iLong = 100L;
+        float iFloat = 3.12F;
+        double iDouble = 17.25D;
+        char iChar = '#';
+        String iString = "Logitech's keyboard";
+
+        System.out.println(a + iByte);
+        System.out.println(iShort - b);
+        System.out.println(iString + " " + "is the best!");
+    }
+}
